de00f6e2749bb903204d976bb7a1efc6477579fc
hemant <hemant1027@gmail.com>
2015-09-04 20:28:08 +0530

    added basic rest calls

--- scripts/app/testRestCtrl.js
+++ scripts/app/testRestCtrl.js
@@ -1,4 +1,20 @@
 angular.module('restTestAPP').controller('testRestCtrl', function ($scope, $http) {
-  $scope.restUrl;
-  console.log('test');
+    //function to be called at time of http call
+    $scope.testRest = function () {
+//        request object to pass in http service
+        var req = {
+            method: "GET",
+            url: $scope.restUrl
+        };
+        console.log(req);
+        $http(req)
+                .success(function (response) {
+                    $scope.response = response;
+                    ;
+                })
+                .error(function (error) {
+                    $scope.error = error;
+                });
+
+    };
 });
