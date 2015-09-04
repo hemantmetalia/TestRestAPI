fb7a927ac424c3d6b8e858aebd27d00a942e34c8
hemant <hemant1027@gmail.com>
2015-09-04 20:33:21 +0530

    added option to select method type

--- scripts/app/testRestCtrl.js
+++ scripts/app/testRestCtrl.js
@@ -1,16 +1,18 @@
 angular.module('restTestAPP').controller('testRestCtrl', function ($scope, $http) {
+    //to hold the rest call type default GET would be selected
+     $scope.method = {};
+    $scope.method.type = 'GET';
     //function to be called at time of http call
     $scope.testRest = function () {
 //        request object to pass in http service
         var req = {
-            method: "GET",
+            method: $scope.method.type,
             url: $scope.restUrl
         };
         console.log(req);
         $http(req)
                 .success(function (response) {
-                    $scope.response = response;
-                    ;
+                    $scope.response = response;                    
                 })
                 .error(function (error) {
                     $scope.error = error;
