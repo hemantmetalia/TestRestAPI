f1ba2bec686fee5d47fcfadb44e578c06389e6d6
hemant <hemant1027@gmail.com>
2015-09-07 18:52:32 +0530

    Updated code to work with parameters passed. and some ui changes.

--- scripts/app/testRestCtrl.js
+++ scripts/app/testRestCtrl.js
@@ -5,45 +5,44 @@ angular.module('restTestAPP').controller('testRestCtrl', function ($scope, $http
     //to store parameters
     $scope.params = [];
     //to store response data
-    $scope.response={};
+    $scope.response = {};
     //function to create input string to send data to http
     var data;
+    //function to set parameters to data object in request
     function setParameters() {
-        data = '{';
+        data = {};
         angular.forEach($scope.params, function (param, index) {
             if (param.name !== undefined)
-                data = data + "'" + param.name + "':'" + param.value + "',";
-        });
-        //to remove last ,
-        data = data.substring(0, data.length - 1);
-        data = data + '}';
-        console.log(data);
-
+                data[param.name] = param.value;
+        });        
     }
     //function to be called at time of http call
     $scope.testRest = function () {
+        //clear response on each new request
+        $scope.response = {};
         setParameters();
 //        request object to pass in http service
         var req = {
             method: $scope.method.type,
             url: $scope.restUrl,
-            data:data
+            data: data
         };
-        console.log(req);
         $http(req)
-                .success(function (data, status, headers, config) {
+                .success(function (data, status, headers, config) {          
                     $scope.response.data = data;
                     $scope.response.status = status;
                     $scope.response.header = headers;
                     $scope.response.config = config;
+                    $scope.response.bgcolor = 'lightgreen';
                 })
                 .error(function (error, status, headers, config) {
-                    
-                    $scope.response.data = "Error "+error.message;
+                    if (error !== null) {
+                        $scope.response.data = "Error " + error.message;
+                    }
                     $scope.response.status = status;
                     $scope.response.header = headers;
                     $scope.response.config = config;
-
+                    $scope.response.bgcolor = '#D86969';
                 });
     };
     //to add more parameters on ui screen
